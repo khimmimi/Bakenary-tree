2464c5c3c5d9a102652cf79abaa32a3d0a8ee90c
khimmimi <Khimlnwza007@gmail.com>
2022-12-13 02:10:04 +0700

    Co-authored-by: Phichaphop Raqsaboon <Ponynie@users.noreply.github.com> Co-authored-by: Nutthapradh Jomsaeng <6434423623@student.chula.ac.th>

--- store/views/payment.py
+++ store/views/payment.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
+
+from store.models.customer import Customer
+from store.models.orders import Order
+from store.models.payslip import Payslip
 from store.models.product import Products
+
 
 class Payment(View):
     def get(self, request):
-        ids = list(request.session.get('cart').keys())
-        products = Products.get_products_by_id(ids)
-        print(products)
-        return render (request, 'payment.html',{'products' : products})
+        customer = request.session.get('customer')
+        payslip=Payslip.get_payslip_by_customer(customer)
+        return render(request, 'payment.html', {'payslip':payslip} )
 
+    def post(self, request):
+        postData = request.POST
+        customer = request.session.get('customer')
+        payslip=Payslip.get_payslip_by_customer(customer)
+        payslip.payslip_image=request.FILES['payslipImage']
+        payslip.getPaySlip()
+        return render(request,'confirm.html')
     
     
 
